728082c8fbd16c383821c900531630f63957257d
Alex17319 <alex@westminstersoftware.com>
2026-06-13 16:32:24 +0930

    Move time zone methods to TimeZoneUtils

--- scripts/index.js
+++ scripts/index.js
@@ -82,12 +82,53 @@ class Event {
     this.whimsicalStartTime = startDateObj && (this.startOnTheHour ? whimsicalMinutelessTimeFormatter.format(startDateObj) : whimsicalMinutefulTimeFormatter.format(startDateObj));
     this.whimsicalEndTime   = endDateObj   && (this.endOnTheHour   ? whimsicalMinutelessTimeFormatter.format(endDateObj)   : whimsicalMinutefulTimeFormatter.format(endDateObj));
 
+    this.utcStartDateObj = TimeZoneUtils.combineDatetimeAndTimezoneAsUTC(startDatetime, timezone);
+    this.utcEndDateObj = TimeZoneUtils.combineDatetimeAndTimezoneAsUTC(endDatetime, timezone);
+
     this.rsvpString = (
       ((this.rsvp || this.rsvpDate) && "RSVP ")
       + (this.rsvp && "to " + this.rsvp)
       + (this.rsvp && this.rsvpDate && " ")
       + (this.rsvpDate && "by " + (this.rsvpMultiYear ? yearfulDateFormatter.format(rsvpDateObj) : yearlessDateFormatter.format(rsvpDateObj)))
     );
+  }
+}
+
+class TimeZoneUtils {
+  static getLocalTimeZone() {
+    // Returns e.g. Australia/Hobart
+    return new Intl.DateTimeFormat().resolvedOptions().timeZone;
+  }
+  
+  static printTimeZone(timeZone, printFormat, locale, referenceDate) {
+    // timeZone should be in the format of e.g. Australia/Hobart
+    // printFormat should be one of "short", "long", "shortOffset", "longOffset", "shortGeneric", or "longGeneric"
+    // locale should be e.g. "en-AU" or defaults to the current locale
+    // referenceDate should be a Date, or defaults to the current date (this impacts whether the daylight-savings-time
+    // vs. standard-time timezone is printed, and reflects political changes e.g. countries switching timezone)
+    // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions
+    if (locale == null) locale = Intl.DateTimeFormat().resolvedOptions().locale;
+    if (referenceDate == null) referenceDate = new Date();
+    return new Intl.DateTimeFormat(locale, {timeZone: timeZone, timeZoneName: printFormat }).formatToParts(new Date(referenceDate)).find(x => x.type === 'timeZoneName').value
+  }
+  
+  static isTimezoneStatic(date1, date2, timeZone) {
+    // returns FALSE if a timezone is discontinuous between date1 and date2 due to a change of daylight savings (or e.g. a country switching timezone for political reasons), otherwise returns TRUE
+    let offset1 = printTimeZone(timeZone, "longOffset", "en-GB", date1);
+    let offset2 = printTimeZone(timeZone, "longOffset", "en-GB", date2);
+    return offset1 === offset2;
+  }
+  
+  static combineDatetimeAndTimezoneAsUTC(datetimeString, timeZone) {
+    // Converts a 'wall clock' date time string plus a timezone string to a UTC time
+    // The timezone specifies a region/city (e.g. Australia/Hobart), which could be in either daylight-savings-time or standard-time
+    // The first attempt gets an approximation of the timezone-offset to use, by pretending the 'wall clock' time is already in UTC (by appending 'Z')
+    // The second attempt uses this approximate timezone-offset to get the correct timezone-offset
+    // Finally (third attempt) a date is returned using the correct timezone-offset
+    // Note: It might technically only need 2 attempts total, I'm not sure. I've tested it out across daylight-savings boundaries and it seems to work OK, so I don't wanna break anything
+    let attempt1 = printTimeZone(timeZone, "longOffset", "fr-FR", new Date(datetimeString + "Z"));
+    let attempt2 = printTimeZone(timeZone, "longOffset", "fr-FR", new Date(datetimeString + attempt1.replace("UTC", "")));
+    return new Date(datetimeString + attempt2.replace("UTC", ""));
   }
 }
 
@@ -128,7 +169,7 @@ const app = Vue.createApp({
   },
   data() {
     return {
-      event: new Event("", "", "", "", this.getLocalTimeZone(), "", "", "", "", this.randomInt(0,999), ""),
+      event: new Event("", "", "", "", TimeZoneUtils.getLocalTimeZone(), "", "", "", "", this.randomInt(0,999), ""),
       urlBase: this.getUrlBase(),
       urlHash: window.location.hash.replace(/^#/, ''),
       urlHashLoaded: false,
@@ -283,38 +324,6 @@ const app = Vue.createApp({
         }
       }
       this.urlHashLoadFailed = true;
-    },
-    getLocalTimeZone() {
-      // Returns e.g. Australia/Hobart
-      return new Intl.DateTimeFormat().resolvedOptions().timeZone;
-    },
-    printTimeZone(timeZone, printFormat, locale, referenceDate) {
-      // timeZone should be in the format of e.g. Australia/Hobart
-      // printFormat should be one of "short", "long", "shortOffset", "longOffset", "shortGeneric", or "longGeneric"
-      // locale should be e.g. "en-AU" or defaults to the current locale
-      // referenceDate should be a Date, or defaults to the current date (this impacts whether the daylight-savings-time
-      // vs. standard-time timezone is printed, and reflects political changes e.g. countries switching timezone)
-      // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/resolvedOptions
-      if (locale == null) locale = Intl.DateTimeFormat().resolvedOptions().locale;
-      if (referenceDate == null) referenceDate = new Date();
-      return new Intl.DateTimeFormat(locale, {timeZone: timeZone, timeZoneName: printFormat }).formatToParts(new Date(referenceDate)).find(x => x.type === 'timeZoneName').value
-    },
-    isTimezoneStatic(date1, date2, timeZone) {
-      // returns FALSE if a timezone is discontinuous between date1 and date2 due to a change of daylight savings (or e.g. a country switching timezone for political reasons), otherwise returns TRUE
-      let offset1 = printTimeZone(timeZone, "longOffset", "en-GB", date1);
-      let offset2 = printTimeZone(timeZone, "longOffset", "en-GB", date2);
-      return offset1 === offset2;
-    },
-    dateTimeStringToUTCString(datetimeString, timeZone) {
-      // Converts a 'wall clock' date time string plus a timezone string to a UTC time
-      // The timezone specifies a region/city (e.g. Australia/Hobart), which could be in either daylight-savings-time or standard-time
-      // The first attempt gets an approximation of the timezone-offset to use, by pretending the 'wall clock' time is already in UTC (by appending 'Z')
-      // The second attempt uses this approximate timezone-offset to get the correct timezone-offset
-      // Finally (third attempt) a date is returned using the correct timezone-offset
-      // Note: It might technically only need 2 attempts total, I'm not sure. I've tested it out across daylight-savings boundaries and it seems to work OK, so I don't wanna break anything
-      let attempt1 = printTimeZone(timeZone, "longOffset", "fr-FR", new Date(datetimeString + "Z"));
-      let attempt2 = printTimeZone(timeZone, "longOffset", "fr-FR", new Date(datetimeString + attempt1.replace("UTC", "")));
-      return new Date(datetimeString + attempt2.replace("UTC", ""));
     },
   },
   computed: {
