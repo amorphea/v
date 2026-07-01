bc079348a7ebd1f39559e3858d3bf22d003ebaec
amorphea <alex@westminstersoftware.com>
2026-07-01 16:11:43 +0930

    Implement shrink text logic

--- scripts/shrink-text.js
+++ scripts/shrink-text.js
@@ -31,6 +31,40 @@ class ShrinkText {
 			return false;
 		}
 
+		ShrinkText.setFontSize(element, 1);
+
+		// First, shrink the font size by halves until the text fits within the max height
+		for (let i = 0; true; i++) {
+			let rect = element.getBoundingClientRect();
+			if (rect.height <= maxHeight) break;
+
+			ShrinkText.setFontSize(element, ShrinkText.getFontSize(element) / 2);
+			
+			if (i >= 10) return false; // Fail if it takes more than 10 iterations
+		}
+
+		// Exit (succeed) if the text didn't shrink at all
+		if (minSize == 1) return true;
+
+		// Then, perform a binary search (4 iterations) to find a good size
+		let minSize = ShrinkText.getFontSize(element);
+		let maxSize = minSize * 2;
+		for (let i = 0; i < 4; i++) {
+			let mean = (minSize + maxSize) / 2;
+			ShrinkText.setFontSize(mean);
+
+			let rect = element.getBoundingClientRect();
+			if (rect.height <= maxHeight) {
+				minSize = mean;
+			} else {
+				maxSize = mean;
+			}
+		}
+		ShrinkText.setFontSize(minSize);
+
+
+
+		
 		var innerSpan, originalHeight, originalHTML, originalWidth;
 		var low, mid, high;
 
@@ -78,5 +112,13 @@ class ShrinkText {
 		// found, updating font if differs:
 		if( innerSpan.style.fontSize != size + 'px' ) innerSpan.style.fontSize = size + 'px';
 
+	}
+
+	static getFontSize(element) {
+		return element.style.fontSize ? Number(element.style.fontSize.replace('em', '')) : null;
+	}
+
+	static setFontSize(element, size) {
+		element.style.fontSize = size + 'em';
 	}
 }
